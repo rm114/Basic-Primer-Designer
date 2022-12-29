1f2d8f93be67723d79b0b40469612dd6d1b0dfaa
rm114 <115485520+rm114@users.noreply.github.com>
2022-12-29 13:12:59 -0500

    Update

--- primer.py
+++ primer.py
@@ -1,22 +1,31 @@
 from Bio.Seq import Seq
+from Bio.SeqUtils import MeltingTemp as mt
+from rich.table import Table
+from rich.console import Console
+from rich import box
 import re
+from rich.prompt import Prompt
 
 #User gives the sequence they want to amplify. Can be any length, must include only A, G, C, and T for GC clamp/melting temp, etc. to work properly.
-FASTA = input('\r\nWelcome to Basic Primer Designer. \r\n\r\n** The sequence must only contain A, G, C, and T nucleotides for certain features to work. \r\n** The sequence must be one continous line. Ensure there are no spaces, symbols, or indents (eg. ¶). \r\n\r\nPlease paste the complete 5\'-3\' FASTA sequence you want to amplify, then press enter:')
-
+#FASTA = input('\r\nWelcome to Basic Primer Designer. \r\n\r\n** The sequence must only contain A, G, C, and T nucleotides for certain features to work. \r\n** The sequence must be one continous line. Ensure there are no spaces, symbols, or indents (eg. ¶). \r\n\r\nPlease paste the complete 5\'-3\' FASTA sequence you want to amplify, then press enter:')
+FASTA = Prompt.ask("[yellow]Welcome to Basic Primer Designer![/yellow] \r\n\r\n[blue]** The sequence must only contain A, G, C, and T nucleotides for certain features to work. \r\n** The sequence must be one continous line. Ensure there are no spaces, symbols, or indents (eg. ¶).[/blue] \r\n\r\n[yellow]Please paste the complete 5\'-3\' FASTA sequence you want to amplify, then press enter:[/yellow]")
 my_seq = Seq(FASTA)
 str_seq = str(my_seq)
 
 #User specifies basic primer length. For the GC clamp primer, this sets the min bp length but not the max.
-primer_sizei = input('\r\nWhat length (in bp) do you want your primer to be (not including res. sites)(20 bp reccomended):')
-primer_size = round(int(primer_sizei))
+primer_sizei = Prompt.ask('\r\n[yellow]What length (in bp) do you want your primer to be (not including res. sites)(press enter for the reccomended 10 bp primer size):[/yellow]', default=10)
+try:
+    primer_size = round(int(primer_sizei))
+except: 
+    print('[red]Error: Invalid primer length given. Please try again with an integer response.[/red]')
+
 
 #nucleotides for res. enzyme site.
 res_site_extra = 'AAAAAA'
 
 #predefined restriction enzymes for ease-of-use: forward primer.
-res_sitefw = input('\r\nWhat restriction site do you want for your FORWARD primer? (If no site, type "none"):')
-if res_sitefw.lower() == 'none':
+res_sitefw = Prompt.ask('\r\nWhat restriction site do you want for your FORWARD primer? (If no site, press enter):')
+if len(res_sitefw) < 1:
     res_sitefw = ''
     res_site_extra = ''
 elif res_sitefw.lower() == 'hind111':
@@ -28,11 +37,11 @@ elif res_sitefw.lower() == 'nhe1':
 elif res_sitefw.lower() == 'sal1':
     res_sitefw = 'GTCGAC'
 else:
-    res_sitefw = input ('\r\n***Error, this restriction enzyme is not in the database yet. Please type the 5\'-3\' recognition site, then press enter (no symbols or spaces):')
+    res_sitefw = Prompt.ask('\r\n[red]***Error, this restriction enzyme is not in the database yet. [/red][yellow]Please type the 5\'-3\' recognition site, then press enter (no symbols or spaces):[/yellow]')
 
 #predefined restriction enzymes for ease-of-use: reverse primer.
-res_siterv = input('\r\nWhat restriction site do you want for your REVERSE primer? (If no site, type "none"):')
-if res_siterv.lower() == 'none':
+res_siterv = input('\r\nWhat restriction site do you want for your REVERSE primer? (If no site, press enter):')
+if len(res_siterv) < 1:
     res_siterv = ''
     res_site_extra = ''
 elif res_siterv.lower() == 'hind111':
@@ -163,41 +172,18 @@ mgcrp_t = re.findall("T", str(gc_reverse_primer))
 #Smaller than 14 nucleotides = Tm= (wA+xT) * 2 + (yG+zC) * 4
 #14 nucleotides or larger = Tm= 64.9 +41*(yG+zC-16.4)/(wA+xT+yG+zC)
 
-if primer_size > 14:
-    m_forward_primer = 64.9 + 41 * (len(mfp_g) + len(mfp_c) - 16.4) / (len(mfp_a) + len(mfp_t) + len(mfp_g) + len(mfp_c))
-    m_reverse_primer = 64.9 + 41 * (len(mrp_g) + len(mrp_c) - 16.4) / (len(mrp_a) + len(mrp_t) + len(mrp_g) + len(mrp_c))
-    if gcf_end > 0:
-        m_gc_forward_primer = 64.9 + 41 * (len(mgcfp_g) + len(mgcfp_c) - 16.4) / (len(mgcfp_a) + len(mgcfp_t) + len(mgcfp_g) + len(mgcfp_c))
-    else:
-        m_gc_forward_primer = 0
-
-    if gcr_end > 0:
-        m_gc_reverse_primer = 64.9 + 41 * (len(mgcrp_g) + len(mgcrp_c) - 16.4) / (len(mgcrp_a) + len(mgcrp_t) + len(mgcrp_g) + len(mgcrp_c))
-    else:
-        m_gc_reverse_primer = 0
-
-else:
-    m_forward_primer = (2 * (len(mfp_a) + len(mfp_t))) + (4 * (len(mfp_g) + len(mfp_c)))
-    m_reverse_primer = (2 * (len(mrp_a) + len(mrp_t))) + (4 * (len(mrp_g) + len(mrp_c)))
-    if gcf_end > 0:
-        m_gc_forward_primer = (2 * (len(mgcfp_a) + len(mgcfp_t))) + (4 * (len(mgcfp_g) + len(mgcfp_c)))
-    else:
-        m_gc_forward_primer = 0
-
-    if gcr_end > 0:
-        m_gc_reverse_primer = (2 * (len(mgcrp_a) + len(mgcrp_t))) + (4 * (len(mgcrp_g) + len(mgcrp_c)))
-    else:
-        m_gc_reverse_primer = 0
-
 #GC content % calculation for each primer.
-gcc_forward_primer = (len(mfp_g) + len(mfp_c)) / (len(mfp_a) + len(mfp_t) + len(mfp_g) + len(mfp_c)) * 100
-gcc_reverse_primer = (len(mrp_g) + len(mrp_c)) / (len(mrp_a) + len(mrp_t) + len(mrp_g) + len(mrp_c)) * 100
-print(gcf_end)
+try:
+    gcc_forward_primer = (len(mfp_g) + len(mfp_c)) / (len(mfp_a) + len(mfp_t) + len(mfp_g) + len(mfp_c)) * 100
+except:
+    gcc_forward_primer = 0
+try:
+    gcc_reverse_primer = (len(mrp_g) + len(mrp_c)) / (len(mrp_a) + len(mrp_t) + len(mrp_g) + len(mrp_c)) * 100
+except:
+    gcc_reverse_primer = 0
 if gcf_end > 0:
-    print('if')
     gcc_gc_forward_primer = (len(mgcfp_g) + len(mgcfp_c)) / (len(mgcfp_a) + len(mgcfp_t) + len(mgcfp_g) + len(mgcfp_c)) * 100
 else:
-    print('else')
     gcc_gc_forward_primer = 0
 
 if gcr_end > 0:
@@ -205,13 +191,60 @@ if gcr_end > 0:
 else:
     gcc_gc_reverse_primer = 0
 
+m_gc_primers = 0
+if mt.Tm_Wallace(Seq(gc_forward_primer)) or mt.Tm_Wallace(Seq(gc_reverse_primer))  == 0:
+    m_gc_primers = 0
+
 #Final output for each primer. GC primer will be blank if no G or C was found in specified bounds.
-print('-----------------------------------------------------------------------------------')
-print ('\r\n>>> FORWARD primer:' + '(' + str(len(forward_primer)) + ' bp)' + ',' + '(Approx. melting temp:' + str(round(m_forward_primer)) + '\xb0C)' + ', (GC content %:' + str(round(gcc_forward_primer)) + ')' + ':' + str(forward_primer))
-print ('>>> REVERSE primer:' + '(' + str(len(reverse_primer)) + ' bp)' + ',' + '(Approx. melting temp:' + str(round(m_reverse_primer)) + '\xb0C)' + ', (GC content %:' + str(round(gcc_reverse_primer)) + ')' + ':' +str(reverse_primer))
-print ('\r\n>>> Alternative GC clamp FORWARD primer (if applicable):' + '(' + str(len(gc_forward_primer)) + ' bp)' + ',' + '(Approx. melting temp:' + str(round(m_gc_forward_primer)) + '\xb0C)' + ', (GC content %:' + str(round(gcc_gc_forward_primer)) + ')' + ':' + str(gc_forward_primer))
-print ('>>> Alternative GC clamp REVERSE primer (if applicable):' + '(' + str(len(gc_reverse_primer)) + ' bp)' + ',' + '(Approx. melting temp:' + str(round(m_gc_reverse_primer)) + '\xb0C)' + ', (GC content %:' + str(round(gcc_gc_reverse_primer)) + ')' + ':' +str(gc_reverse_primer))
-print ('\r\nTotal PCR product amplicon size:' + '[' + str(len(amplicon)) + ' bp]')
-print('\r\n-----------------------------------------------------------------------------------')
+print(res_sitefw)
+print(res_siterv)
+gc_style_fw = "green"
+gc_style_rv = "green"
+gc_seq_fw = ""
+gc_seq_rv = ""
+gc_fw_five_prime = "5'__"
+gc_fw_three_prime = "__3'"
+gc_rv_five_prime = "5'__"
+gc_rv_three_prime = "__3'"
+gc_res_sitefw = ""
+gc_res_siterv = ""
+if len(gc_forward_primer) < 1:
+    gc_style_fw = "red"
+    gc_seq_fw = "N/A"
+    gc_fw_five_prime = ""
+    gc_fw_three_prime = ""
+    gc_res_sitefw = "N/A"
+else:
+    gc_style_fw = gc_forward_primer
+    gc_res_sitefw = res_sitefw
+
+if len(gc_reverse_primer) < 1:
+    gc_style_rv = "red"
+    gc_seq_rv = "N/A"
+    gc_rv_five_prime = ""
+    gc_rv_three_prime = ""
+    gc_res_siterv = "N/A"
+else:
+    gc_seq_rv = gc_reverse_primer
+    gc_res_siterv = res_siterv
+    
+
+table = Table(title="[bold]Generated Primers[/bold]", caption=f'[bold]Reference Sequence[/bold]:\n 5\'__{str_seq}__3\'', show_lines=1)
+table.add_column("Primer Orientation")
+table.add_column("Sequence (5'->3')", overflow="fold")
+table.add_column("Restriction Site")
+table.add_column("Length (bp)")
+table.add_column("Approx. Melting Point (\xb0C)")
+table.add_column("GC Content (%)")
+table.add_row("[yellow]FORWARD[/yellow]", f"'5__{str(forward_primer)}__3'", res_sitefw, str(len(forward_primer)), str(mt.Tm_Wallace(Seq(forward_primer))), str(round(gcc_forward_primer)), style="green")
+table.add_row("[yellow]REVERSE[/yellow]", f"'5__{str(reverse_primer)}__3'", res_siterv, str(len(reverse_primer)), str(mt.Tm_Wallace(Seq(reverse_primer))), str(round(gcc_reverse_primer)), style="green")
+table.add_row("[yellow]GC Clamp FORWARD (if applicable)[/yellow]", f"{gc_fw_five_prime}{gc_seq_fw}{gc_fw_three_prime}", gc_res_sitefw, str(len(gc_forward_primer)), str(mt.Tm_Wallace(Seq(gc_forward_primer))), str(round(gcc_gc_forward_primer)), style=gc_style_fw)
+table.add_row("[yellow]GC Clamp REVERSE(if applicable)[/yellow]", f"{gc_rv_five_prime}{gc_seq_rv}{gc_rv_three_prime}", gc_res_siterv, str(len(gc_reverse_primer)), str(mt.Tm_Wallace(Seq(gc_reverse_primer))), str(round(gcc_gc_reverse_primer)), style=gc_style_rv)
+
+
+console = Console()
+print('\n\n' + '-' * 120)
+console.print(table)
+print('\n' + '-' * 120)
 
 quit()
